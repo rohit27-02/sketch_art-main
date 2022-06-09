b85c5fb173422ab2075a9be5f2f75ea96052ad87
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-06-09 15:49:47 +0530

    commit

--- pages/about.js
+++ pages/about.js
@@ -1,59 +1,76 @@
+/* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
+import Head from 'next/head'
+import { Fade, LightSpeed, Slide } from 'react-reveal';
 const About = () => {
   
   return (
+  
     <div>
-      <div >
-<img className='overflow-hidden relative top-0' src='https://pbs.twimg.com/media/FO8RsG4VcAU2mHZ?format=jpg&name=large'></img>
-<div className=' w-full absolute  top-56 animate-fade-in-down  text-white text-3xl md:text-5xl font-extrabold text-center'>About Us</div>
+        <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"/>
+        </Head>
+
+      <div style={{height:"130vh"}} className='w-full flex justify-evenly flex-col  items-center'>
+        <h1 style={{fontFamily:"'poppins',sans-serif"}} className='text-4xl font-bold'>How our journey began</h1>
+<img style={{height:"70vh",width:"auto"}} className='overflow-hidden  relative top-0' src='/b.jpg'></img>
       </div>
-      <div className='text-center md:py-20 py-8 px-1 bg-slate-50 md:flex-row flex flex-col justify-center items-center '>
-       <div> <h1 className='text-xl font-bold'>GLOBAL LEADING MANUFACTURER</h1>
-        <p className='text-left  py-7 px-7 font-thin'>With an industry experience of over 25 years, sketch arts private limited is the leading manufacturer and importer of Window Blinds and Accessories, Motorized Blinds and Interior Flims in the country catering to clients across the globe. It is also the marketing partner for sketch arts based in China. Keen on widening its portfolio, sketch arts is open to expanding its imports and exports as and when opportunities present themselves.</p>
-<p className='text-left  py-7 px-7 font-thin -my-6' >
-sketch arts strives for excellence by providing customized solutions. An expansive range of world-class home fashion products while providing premium customer services that include global sourcing, quality assurance, logistics and finance.</p>
-<button className='text-xl p-5 font-bold hover:text-yellow-500'>Contact Us</button>
-</div>
-<img className='p-10' width={500} height={500} src='https://blackoutcurtainabudhabi.com/wp-content/uploads/2020/12/Curtain-6.jpg'></img>
-      </div>
-      <div className='flex flex-col md:flex-row  justify-around items-center leading-10 bg-yellow-300  '>
-<img width={500} height={500} className='p-16 ' src='https://www.lvtron.com/wp-content/uploads/2019/09/1591582913-5.jpg'></img>
-<div><h1 className='font-bold text-xl'>OUR CAPABILITIES</h1>
-<ul className='list-disc font-thin my-4'>
-  <li>Team of expert inspectors</li>
-  <li>Team of experienced procurement agents</li>
-  <li>Reliable vendor base</li>
-  <li>Competitive prices </li>
-  <li>Wide Distribution Network</li>
-  <li>Timely deliver of consignment</li>
-</ul></div>
-      </div>
-      <div  className='text-center md:py-10 py-8 px-1 bg-slate-50 md:flex-row flex flex-col justify-center items-center '>
+      <div style={{backgroundColor:"#bfb1c4",fontFamily:"'poppins', sans-serif"}} className='text-center md:py-10 py-8 px-1 bg-slate-50 md:flex-row flex flex-col justify-center items-center '>
         <div>
-          <h1 className='text-xl font-bold'>HOW WE DO IT</h1>
-          <p className='text-left  py-7 px-7 font-thin'>A systemised process ensures that we deliver only the best.
+         <Slide bottom> <h1 style={{fontFamily:"'poppins', sans-serif"}} className='text-3xl  font-bold'>How we do it</h1></Slide>
+         <Slide bottom><p className='  py-7 px-7 font-thin text-xl'>A systemised process ensures that we deliver only the best.
 In the commitment of importing top-notch quality products
 from authentic vendors only, we have employed a distinguished
 team of proficient procurement agents who incurs years of
 experience in this business domain. This team selects the most
-genuine and experienced vendors after an in-depth research.</p>
+genuine and experienced vendors after an in-depth research.</p></Slide>
         </div>
-        <img width={500} height={500} className="p-8" src='https://media.istockphoto.com/photos/little-boy-opening-the-roller-blinds-in-the-morning-picture-id1178700357?k=20&m=1178700357&s=612x612&w=0&h=zZMixHxDvjJj4ce_fEjPEFE9ReFw5xabNAPh3Rypdxo='></img>
+        <Slide right><img width={500} height={500} className="p-8" src='https://media.istockphoto.com/photos/little-boy-opening-the-roller-blinds-in-the-morning-picture-id1178700357?k=20&m=1178700357&s=612x612&w=0&h=zZMixHxDvjJj4ce_fEjPEFE9ReFw5xabNAPh3Rypdxo='></img></Slide>
       </div>
+      <div style={{fontFamily:"'poppins', sans-serif"}} className='flex flex-col md:flex-row bg-white justify-around items-center leading-10  '>
+<img style={{height:600}} className='w-5/12  my-10' src='https://www.lvtron.com/wp-content/uploads/2019/09/1591582913-5.jpg'></img>
+<div><h1 className='font-bold text-3xl'>Our Capabilities</h1>
+<ul className='list-disc text-xl space-y-4  my-16'>
+  <LightSpeed><li>Team of expert inspectors</li></LightSpeed>
+  <LightSpeed><li>Team of experienced procurement agents</li></LightSpeed>
+  <LightSpeed><li>Reliable vendor base</li></LightSpeed>
+  <LightSpeed><li>Competitive prices </li></LightSpeed>
+  <LightSpeed><li>Wide Distribution Network</li></LightSpeed>
+  <LightSpeed><li>Timely deliver of consignment</li></LightSpeed>
+</ul></div>
+      </div>
+
+      <div style={{height:"50vh"}} className="flex justify-center">
+      <video className=" w-11/12 object-cover" src="/main2.mp4" autoPlay muted loop />
+      <h1 style={{fontFamily:"'poppins', sans-serif",textShadow:"1px 1px black"}} className='absolute text-white text-5xl text-center px-40 my-20'>Sketch arts strives for excellence by providing customized solutions.</h1>
+      </div>
+
+      
+      <div  style={{fontFamily:"'poppins', sans-serif"}} className='text-center md:py-20 py-8 px-10  text-slate-700 bg-slate-50 md:flex-row flex flex-col justify-center items-center '>
+       <div> <h1 className='text-2xl font-bold'>Global Leading Manufacturer</h1>
+        <p className=' py-7 px-7 text-2xl font-thin'><i className='text-3xl font-bold mr-4'>"</i> With an industry experience of over 25 years, sketch arts private limited is the leading manufacturer and importer of Window Blinds and Accessories, Motorized Blinds and Interior Flims in the country catering to clients across the globe. It is also the marketing partner for sketch arts based in China. Keen on widening its portfolio, sketch arts is open to expanding its imports and exports as and when opportunities present themselves. <i className='text-3xl font-bold mr-4'>"</i></p>
+<button style={{backgroundColor:"#bfb1c4"}} className='text-xl hover:opacity-80 p-5 font-bold'>Contact Us</button>
+</div>
+
+      </div>
+    
+     
       <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
         {({ isVisible }) => (
-           <div className='w-full md:h-56 h-96 bg-slate-700'>
-           {isVisible ?<div  className='h-full flex flex-col md:flex-row bg-slate-700 text-white items-center justify-evenly'> <CountUp
+           <div style={{backgroundColor:"#ebeaeb"}} className='w-full md:h-56 h-96 '>
+           {isVisible ?<div style={{fontFamily:"'poppins', sans-serif",backgroundColor:"#bfb1c4"}} className='h-full flex flex-col md:flex-row  text-black items-center justify-evenly'> <CountUp
            className='text-4xl font-bold  py-5'
        start={0}
        end={20}
        duration={2}
        separator=" "
-       suffix='+ YEARS'
+       suffix='+ Years'
      
        
      ></CountUp>
@@ -63,7 +80,7 @@ genuine and experienced vendors after an in-depth research.</p>
        end={50}
        duration={2}
        separator=" "
-       suffix='+ EXPERTS'
+       suffix='+ Experts'
      
        
      ></CountUp>
@@ -73,7 +90,7 @@ genuine and experienced vendors after an in-depth research.</p>
        end={5000}
        duration={2}
        separator=", "
-       suffix='+ PROJECTS'
+       suffix='+ Projects'
      
        
      ></CountUp>
@@ -83,7 +100,7 @@ genuine and experienced vendors after an in-depth research.</p>
        end={2000}
        duration={2}
        separator=" ,"
-       suffix='+ CLIENTS'
+       suffix='+ Clients'
      
        
      ></CountUp></div>:null}
--- pages/index.js
+++ pages/index.js
@@ -132,11 +132,15 @@ export default function Home() {
     <div className=" md:bg-white rounded-lg md:p-8 flex flex-col md:flex-row  items-center md:space-x-5 md:ml-auto w-full relative z-10 md:shadow-md">
       <div className="relative mb-4">
         <label htmlFor="Full Name" className="leading-7 text-sm text-gray-600">Name</label>
-        <input id="Full Name" name="Full Name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm md:text-base outline-none text-gray-700 py-1 px-3 md:leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text" id="Full Name" name="Full Name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm md:text-base outline-none text-gray-700 py-1 px-3 md:leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <div className="relative mb-4">
         <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
         <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm md:text-base outline-none text-gray-700 py-1 px-3 md:leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <div className="relative mb-4">
+        <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
+        <input type="tel" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm md:text-base outline-none text-gray-700 py-1 px-3 md:leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <button className="text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 rounded text-sm md:text-lg">Send</button>
     </div>
