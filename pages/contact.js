fe2b97aa5f8eedaf4cae4764f890bfc506aefa8f
Your Name <you@example.com>
2022-06-11 18:18:50 +0530

    c

--- pages/contact.js
+++ pages/contact.js
@@ -6,6 +6,9 @@ import mongoose from "mongoose";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from 'react';
+import {GrMapLocation} from "react-icons/gr"
+import {AiOutlineFlag} from "react-icons/ai"
+import {IoIosPhonePortrait} from "react-icons/io"
 import Router from 'next/router';
 
 const Contact = ({ info }) => {
@@ -52,8 +55,73 @@ const Contact = ({ info }) => {
       }, 3000);
     }
   }
-  return (
-    <section className=" body-font relative">
+  return (<>
+  
+  <div style={{height:"70vh"}} className="overflow-hidden absolute z-10  top-0 bg-black w-full">
+    <img src='/pexels-angela-roma-7319189.jpg' className='object-cover h-full w-full'></img>
+  </div>
+  <div style={{height:"100vh"}}>
+  <div style={{height:"170vh"}} className="flex items-center w-full">
+    <div className='flex justify-evenly px-28 w-full'>
+       <div className='text-center  flex space-y-5 flex-col justify-center items-center w-1/3'> 
+        <GrMapLocation className='text-5xl'/>
+        <h1>Our address</h1>
+        <p>{info[0].address}</p>
+      </div>
+       <div className='text-center  flex space-y-5 flex-col justify-center items-center w-1/3'> 
+        <AiOutlineFlag className='text-5xl'/>
+        <h1>Our email</h1>
+        <p>{info[0].email}</p>
+      </div>
+       <div className='text-center  flex space-y-5 flex-col justify-center items-center w-1/3'>
+        <IoIosPhonePortrait className='text-5xl'/>
+        <h1>Call us</h1>
+        <p>{info[0].phone}</p>
+        </div>
+    </div>
+  </div>
+  </div>
+
+<section  style={{ fontFamily: "'Poppins', sans-serif" }} className="text-gray-600 body-font relative">
+  <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+    <div className="lg:w-1/2 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+      
+     
+    </div>
+    <div className="lg:w-1/2 md:w-1/2 bg-white flex space-y-8 flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+      <h2 className="text-gray-900 text-3xl mb-1 font-medium title-font">Leave a comment</h2>
+      
+      <div className="flex space-x-5 relative mb-4">
+      <div >
+        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+        <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <div >
+        <label htmlFor="company" className="leading-7 text-sm text-gray-600">company</label>
+        <input type="text" id="company" name="company" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      </div>
+      <div className="flex space-x-5 relative mb-4">
+      <div >
+        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <div >
+        <label htmlFor="number" className="leading-7 text-sm text-gray-600">number</label>
+        <input type="tel" id="number" name="number" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      </div>
+      <div className="relative mb-4">
+        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      </div>
+     
+      <button className="text-white mx-72 bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">SUMBIT</button>
+    </div>
+  </div>
+</section>
+
+    {/*<section className=" body-font relative">
       <ToastContainer
         position="top-center"
         autoClose={5000}
@@ -108,7 +176,8 @@ const Contact = ({ info }) => {
         </div>
       </section>
      
-    </section>
+    </section>*/}
+    </>
   )
 }
 export async function getStaticProps(context) {
