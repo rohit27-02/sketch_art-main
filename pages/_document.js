ad4340f53a487c7c836b2540fbc4347596db8156
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-06-17 21:06:30 +0530

    c

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Montserrat:wght@300&family=Poppins:wght@500&family=Fjalla+One&family=Roboto+Slab:wght@300&display=swap" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Montserrat:wght@300&family=Poppins:wght@500&family=Fjalla+One&family=Roboto+Slab:wght@300&family=Playfair+Display&display=swap" rel="stylesheet"/>
         
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
