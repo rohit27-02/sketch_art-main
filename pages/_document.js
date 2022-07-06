8716350e5e2f58e6e781e40834204dc8a40ebb26
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-07-06 09:43:55 +0530

    c

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
 
- 
+
   static async getInitialProps(ctx) {
    
     const originalRenderPage = ctx.renderPage
