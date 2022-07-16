c29fb6c81e2443829ac0c95f8a44ed6bcaf1c314
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-07-16 13:00:19 +0530

    c

--- pages/Checkout.js
+++ pages/Checkout.js
@@ -4,6 +4,7 @@ import { CgTrashEmpty } from 'react-icons/cg';
 import {MdOutlinePayment} from "react-icons/md"
 import {useEffect, useState } from 'react';
 
+
 const Checkout = ({cart,removeFromCart,addToCart,subTotal}) => {
     const [sw, setsw] = useState(false);
     const [name, setname] = useState(false);
@@ -40,48 +41,59 @@ const Checkout = ({cart,removeFromCart,addToCart,subTotal}) => {
         }
       }
 
-    const initiatepayment= async ()=>{
-        let email=email
-        let Oid = Math.floor(Math.random()*Date.now())
-        const data ={cart,subTotal,Oid,email}
-       const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pretransaction`,
-       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        }, body: JSON.stringify(data)
-      })
-     
-       let txnToken = await res.json()
-       console.log(txnToken)
-        var config = {
-            "root": "",
-            "flow": "DEFAULT",
-            "data": {
-            "orderId":Oid, 
-            "token": txnToken, /* update token value */
-            "tokenType": "TXN_TOKEN",
-            "amount": subTotal /* update amount */
-            },
-            "handler": {
-              "notifyMerchant": function(eventName,data){
-                console.log("notifyMerchant handler function called");
-                console.log("eventName => ",eventName);
-                console.log("data => ",data);
-              } 
-            }
+      const initializeRazorpay = () => {
+        return new Promise((resolve) => {
+          const script = document.createElement("script");
+          script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    
+          script.onload = () => {
+            resolve(true);
           };
-          
+          script.onerror = () => {
+            resolve(false);
+          };
+    
+          document.body.appendChild(script);
+        });
+      };
 
-                  window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
-                      // after successfully updating configuration, invoke JS Checkout
-                      window.Paytm.CheckoutJS.invoke();
-                  }).catch(function onError(error){
-                      console.log("error => ",error);
-                  });
-                  
-              
-    }
+      const makePayment = async () => {
+        const res = await initializeRazorpay();
+    
+        if (!res) {
+          alert("Razorpay SDK Failed to load");
+          return;
+        }
+    
+        // Make API call to the serverless API
+        const data = await fetch("/api/razorpay", { method: "POST" }).then((t) =>
+          t.json()
+        );
+        console.log(data);
+        var options = {
+          key: "rzp_test_VlpBraARgWRC8D", // Enter the Key ID generated from the Dashboard
+          name: "Sketch Art",
+          currency: "INR",
+          amount: subTotal,
+          order_id: data.id,
+          description: "Thankyou for purchasing with us",
+          image: "/logo.jpeg",
+          handler: function (response) {
+            // Validate payment at server - using webhooks is a better idea.
+            alert(response.razorpay_payment_id);
+            alert(response.razorpay_order_id);
+            alert(response.razorpay_signature);
+          },
+          prefill: {
+            name: "Manu Arora",
+            email: "manuarorawork@gmail.com",
+            contact: "9999999999",
+          },
+        };
+    
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+      };
 
     useEffect(() => {
         if(screen.width>768){
